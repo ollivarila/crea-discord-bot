48a529231ea22317e6a58c0037c5e08c9be5dc24
ollivarila <olli.varila@gmail.com>
2022-11-11 15:32:02 +0200

    subscriber dao first version

---
+++ dao/subscriberDao.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+import Subscriber from '../models/Subscriber.js'
+
+const get = async () => {
+
+}
+
+const create = async () => {
+
+}
+
+const update = async () => {
+
+}
+
+const remove = async () => {
+
+}
+
+const getAll = async () => {
+
+}
+
+export default {
+  get,
+  create,
+  update,
+  remove,
+  getAll
+}
