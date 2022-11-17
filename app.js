1e4651c58a328d9d1487a1649236ff1e8b5daaf6
ollivarila <olli.varila@gmail.com>
2022-11-17 22:42:03 +0200

    axios mock

--- app.js
+++ app.js
@@ -10,7 +10,14 @@ const jobController = require('./controllers/jobController')
 const config = require('./config')
 const Challenge = require('./models/Challenge')
 
+const {
+  PUBLICKEY, GUILDID, APPID, DISCORDTOKEN, WEATHERTOKEN,
+} = process.env
 const app = express()
+
+if (!(PUBLICKEY && GUILDID && APPID && DISCORDTOKEN && WEATHERTOKEN)) {
+  throw new Error('Environment variables not set correctly')
+}
 
 mongoose.connect(config.MONGODB_URI).then(async () => {
   info('Connected to MongoDB')
--- tests/commands/commands.test.js
+++ tests/commands/commands.test.js
@@ -1,31 +1,56 @@
 /* eslint-disable no-console */
 /* eslint-disable no-undef */
 const mongoose = require('mongoose')
+const axios = require('axios')
+const MockAdapter = require('axios-mock-adapter')
 const { createDmChannel, subscribeUser } = require('../../commands/subscribe')
-const { discordRequest } = require('../../utils/requests')
 const { getPP } = require('../../commands/pp')
 const config = require('../../config')
 const { info, error } = require('../../utils/logger')
 
 describe('Subscribe tests', () => {
+  let mock = new MockAdapter(axios)
   const discordid = '188329879861723136'
 
-  beforeAll(async () => mongoose.connect(config.MONGODB_URI)
-    .then(() => {
-      info('connected to mongodb')
-    })
-    .catch(err => error(err)))
+  beforeAll(async () => {
+    mock = new MockAdapter(axios)
+    return mongoose.connect(config.MONGODB_URI)
+      .then(() => {
+        info('connected to mongodb')
+      })
+      .catch(err => error(err))
+  })
 
   afterEach(async () => {
-    const endpoint = '/channels/1041324752293347358'
-    await discordRequest(endpoint, {
-      method: 'delete',
-    })
+    mock.reset()
   })
 
   test('createDmChannel()', async () => {
+    const mockRes = {
+      data: {
+        id: '1041324752293347358',
+        type: 1,
+        last_message_id: '1042432599638949938',
+        flags: 0,
+        recipients: [
+          {
+            id: '188329879861723136',
+            username: 'LMAO',
+            avatar: '73635435b3a4ed04a23ff50c4fd04e74',
+            avatar_decoration: null,
+            discriminator: '0157',
+            public_flags: 640,
+          },
+        ],
+      },
+    }
+    const url = 'https://discord.com/api/v10/users/@me/channels'
+    mock.onPost(url).reply(200, mockRes.data)
+
     const res = await createDmChannel(discordid)
-    expect(res.recipients[0].username).toBe('Crea')
+    // expect(res.recipients[0].username).toBe('Crea')
+    console.log(res)
+    expect(res).toEqual(mockRes.data)
   })
 
   test('subscribeUser()', async () => {
@@ -36,6 +61,32 @@ describe('Subscribe tests', () => {
       time: '8:00',
       utcOffset: 1,
     }
+    const url = /https:\/\/api.openweathermap.org*/
+    const mockRes = {
+      data: {
+        id: '1041324752293347358',
+        type: 1,
+        last_message_id: '1042432599638949938',
+        flags: 0,
+        recipients: [
+          {
+            id: '188329879861723136',
+            username: 'LMAO',
+            avatar: '73635435b3a4ed04a23ff50c4fd04e74',
+            avatar_decoration: null,
+            discriminator: '0157',
+            public_flags: 640,
+          },
+        ],
+      },
+    }
+    const url2 = 'https://discord.com/api/v10/users/@me/channels'
+    mock.onPost(url, undefined, {
+      Authorization: expect.not.stringMatching(/undefined/),
+      'User-Agent': expect.stringContaining('Discord bot'),
+    }).reply(200, mockRes.data)
+
+    mock.onGet(url).reply(200, { list: [1, 2, 3] })
     const res = await subscribeUser(user, (data) => {
       console.log(data)
     })
