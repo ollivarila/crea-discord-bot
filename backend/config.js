3c698bc65057d126698299725b8894b230f8b5eb
ollivarila <olli.varila@gmail.com>
2022-12-17 18:53:11 +0200

    added development db url

--- backend/config.js
+++ backend/config.js
@@ -1,11 +1,15 @@
+/* eslint-disable no-nested-ternary */
 const dotenv = require('dotenv')
 
 dotenv.config()
 
-const MONGODB_URI = process.env.NODE_ENV === 'test'
-  ? process.env.TEST_MONGODB_URI
-  : process.env.MONGODB_URI
+const MONGODB_URI =
+	process.env.NODE_ENV === 'test'
+		? process.env.TEST_MONGODB_URI
+		: process.env.NODE_ENV === 'development'
+		? process.env.MONGODB_URI
+		: process.env.DEV_MONGODB_URI
 
 module.exports = {
-  MONGODB_URI,
+	MONGODB_URI,
 }
