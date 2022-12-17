76ed085211f4307d48485c54ee6873f6f2557402
ollivarila <olli.varila@gmail.com>
2022-12-17 17:21:11 +0200

    Changed discordrequest func #patch

--- backend/utils/requests.js
+++ backend/utils/requests.js
@@ -8,63 +8,67 @@ dotenv.config()
 const { APPID, GUILDID, DISCORDTOKEN } = process.env
 
 async function checkLimit(res) {
-  const limitRemaining = res.headers['x-ratelimit-remaining']
-  const timeUntilReset = res.headers['x-ratelimit-reset-after']
-  if (limitRemaining === undefined) return
-  if (parseInt(limitRemaining, 10) === 0) {
-    info(`Limit reached sleeping for ${timeUntilReset}s`);
-    await sleep(timeUntilReset * 1000 + 100)
-  }
+	const limitRemaining = res.headers['x-ratelimit-remaining']
+	const timeUntilReset = res.headers['x-ratelimit-reset-after']
+	if (limitRemaining === undefined) return
+	if (parseInt(limitRemaining, 10) === 0) {
+		info(`Limit reached sleeping for ${timeUntilReset}s`)
+		await sleep(timeUntilReset * 1000 + 100)
+	}
 }
 
 async function discordRequest(endpoint, options) {
-  const baseurl = 'https://discord.com/api/v10'
-  const url = baseurl + endpoint
-  const headers = {
-    Authorization: `Bot ${DISCORDTOKEN}`,
-    'User-Agent': 'DiscordBot (1.0.0)',
-  }
-  const res = await axios.request({
-    url,
-    headers,
-    ...options,
-  }).catch(err => {
-    error('Error with discord request', err)
-    return null
-  })
+	const baseurl = 'https://discord.com/api/v10'
+	const url = baseurl + endpoint
+	const headers = {
+		Authorization: `Bot ${DISCORDTOKEN}`,
+		'User-Agent': 'DiscordBot (1.0.0)',
+		'Accept-encoding': '*',
+	}
+	const res = await axios
+		.request({
+			url,
+			headers,
+			...options,
+		})
+		.catch(err => {
+			error('Error with discord request', err)
+			return null
+		})
 
-  if (!res) return null
+	if (!res) return null
 
-  if (res.status === 204 && res.config.method === 'delete') return true
+	if (res.status === 204 && res.config.method === 'delete') return true
 
-  await checkLimit(res)
-  return res.data
+	await checkLimit(res)
+	return res.data
 }
 
 const installCommand = async command => {
-  const endpoint = `/applications/${APPID}/guilds/${GUILDID}/commands`
-  await discordRequest(endpoint, {
-    method: 'post',
-    data: command,
-  })
-    .then(() => info(`Installed command ${command.name}`))
-    .catch(() => error('Error installing command'))
+	const endpoint = `/applications/${APPID}/guilds/${GUILDID}/commands`
+	await discordRequest(endpoint, {
+		method: 'post',
+		data: command,
+	})
+		.then(() => info(`Installed command ${command.name}`))
+		.catch(() => error('Error installing command'))
 }
 
 async function request(url, options) {
-  return axios.request({
-    url,
-    ...options,
-  })
-    .then(res => res.data)
-    .catch(err => {
-      error('Error with request', err.message)
-      return null
-    })
+	return axios
+		.request({
+			url,
+			...options,
+		})
+		.then(res => res.data)
+		.catch(err => {
+			error('Error with request', err.message)
+			return null
+		})
 }
 
 module.exports = {
-  installCommand,
-  request,
-  discordRequest,
+	installCommand,
+	request,
+	discordRequest,
 }
