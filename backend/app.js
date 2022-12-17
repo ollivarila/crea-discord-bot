6a6cafdc6d11a649943fd98beb8b852849de2bf2
ollivarila <olli.varila@gmail.com>
2022-12-17 18:36:51 +0200

    refactoring

--- backend/app.js
+++ backend/app.js
@@ -4,9 +4,9 @@ const interactionRouter = require('./controllers/interactionRouter')
 const { info } = require('./utils/logger')
 const onStartUp = require('./utils/startUp')
 const {
-  interactionExtractor,
-  interactionLogger,
-  requestLogger,
+	interactionExtractor,
+	interactionLogger,
+	requestLogger,
 } = require('./utils/middleware')
 const { recordStatistics } = require('./controllers/statsController')
 
@@ -14,7 +14,7 @@ const { PUBLICKEY, APPID, DISCORDTOKEN, WEATHERTOKEN, PORT } = process.env
 const app = express()
 
 if (!(PUBLICKEY && APPID && DISCORDTOKEN && WEATHERTOKEN)) {
-  throw new Error('Environment variables not set correctly')
+	throw new Error('Environment variables not set correctly')
 }
 
 onStartUp().then(() => info('startup completed'))
@@ -22,35 +22,27 @@ onStartUp().then(() => info('startup completed'))
 app.use(requestLogger)
 // For health checks
 app.get('/health', (req, res) => {
-  res.send('ok')
-})
-
-// To see if version has actually updated
-app.get('/version', (req, res) => {
-  res.send('1')
+	res.send('ok')
 })
 
 // Parse request body and verifies incoming requests using discord-interactions package
-if (process.env.NODE_ENV !== 'test' && process.env.NODE_ENV !== 'test:debug') {
-  app.use(
-    '/interactions',
-    express.json({ verify: verifyDiscordRequest(process.env.PUBLICKEY) }),
-  )
+if (process.env.NODE_ENV !== 'test') {
+	app.use(
+		'/interactions',
+		express.json({ verify: verifyDiscordRequest(process.env.PUBLICKEY) }),
+	)
 } else {
-  app.use(express.json())
+	app.use(express.json())
 }
 
 app.use('/interactions', interactionExtractor)
 app.use('/interactions', interactionLogger)
 
-/**
- * Interactions endpoint URL where Discord will send HTTP requests
- */
 app.use('/interactions', recordStatistics)
 app.use('/interactions', interactionRouter)
 
 if (process.env.NODE_ENV !== 'test') {
-  app.listen(PORT || 3000, () => info(`Listening on port ${PORT}`))
+	app.listen(PORT || 3000, () => info(`Listening on port ${PORT}`))
 }
 
 module.exports = app
--- backend/utils/startUp.js
+++ backend/utils/startUp.js
@@ -4,14 +4,20 @@ const { setUpSubscriptions } = require('../commands/subscribe')
 const config = require('../config')
 const { info, error } = require('./logger')
 
-const connectDb = async () => mongoose.connect(config.MONGODB_URI).then(() => {
-  info('Connected to MongoDB')
-}).catch(err => error('Error connecting to MongoDB', err))
+const connectDb = async () =>
+	mongoose
+		.connect(config.MONGODB_URI)
+		.then(() => {
+			info('Connected to MongoDB')
+		})
+		.catch(err => error('Error connecting to MongoDB', err))
 
 const onStartUp = async () => {
-  await connectDb()
-  await setUpSubscriptions()
-  await setUpLeaderboards()
+	await connectDb()
+	if (process.env.NODE_ENV === 'production') {
+		await setUpSubscriptions()
+		await setUpLeaderboards()
+	}
 }
 
 module.exports = onStartUp
